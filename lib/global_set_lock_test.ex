f5705b06cda2d5a084f0ad83d1c4050fa3aed044
Ryota Kinukawa <pojiro.jp@gmail.com>
2022-01-07 16:57:57 +0900

    feat: first commit

---
+++ lib/global_set_lock_test.ex
@@ -0,0 +1,18 @@
+defmodule GlobalSetLockTest do
+  @moduledoc """
+  Documentation for `GlobalSetLockTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GlobalSetLockTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/global_set_lock_test_test.exs
@@ -0,0 +1,8 @@
+defmodule GlobalSetLockTestTest do
+  use ExUnit.Case
+  doctest GlobalSetLockTest
+
+  test "greets the world" do
+    assert GlobalSetLockTest.hello() == :world
+  end
+end
